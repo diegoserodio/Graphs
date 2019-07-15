c927df043232e4fed4eec11ebbc4af8215ba5d84
diegoserodio <diegocosta2113@gmail.com>
2019-07-15 09:43:19 -0300

    Updates for working with google spreadsheets

--- libraries/graph.js
+++ libraries/graph.js
@@ -50,8 +50,6 @@ class Graph{
   show(){
     var links_qtn = 0;
     for(var i = 0; i < this.node.length; i++){
-      fill(255, 255, 255);
-      ellipse(this.node[i].position.x, this.node[i].position.y, this.node[i].size, this.node[i].size);
       for(var j = 0; j < this.node[i].links.length; j++){
         this.link[links_qtn] = new Link(this.node[i], this.node[i].links[j]);
         stroke(255, 255, 255);
@@ -60,7 +58,13 @@ class Graph{
         this.drawArrow(this.node[i], this.node[i].links[j]);
         links_qtn++;
       }
+      fill(255, 255, 255);
+      stroke(0,0,0);
+      ellipse(this.node[i].position.x, this.node[i].position.y, this.node[i].size);
+      fill(0, 0, 255);
+      text(i, this.node[i].position.x-this.node[i].size/2, this.node[i].position.y-this.node[i].size/2);
     }
+    this.dominanceArea();
   }
 
   drawArrow(a, b){
@@ -76,12 +80,46 @@ class Graph{
       y_orientation = 1;
     }
     var theta = Math.atan(Math.abs((b.position.y-a.position.y)/(b.position.x-a.position.x)));
-    var radius = (b.links.length+1)*6;
+    var radius = b.size-8;
     var x = b.position.x + radius*Math.cos(theta)*x_orientation;
     var y = b.position.y + radius*Math.sin(theta)*y_orientation;
     fill(255, 0, 0);
     noStroke();
-    ellipse(x, y, 6, 6);
+    ellipse(x, y, 6);
+  }
+
+  dominanceArea(){
+    var delta_d = 1;
+    for(var i = 0; i < this.node.length; i++){
+      noStroke();
+      fill(50, 50, 50);
+      if(this.node[i].links.length != 0){
+        for(var j = i; j < this.node.length; j++){
+          if(this.node[j].links.length != 0){
+            var delta_x = this.node[i].position.x-this.node[j].position.x;
+            var delta_y = this.node[i].position.y-this.node[j].position.y;
+            var dist = Math.sqrt(delta_x*delta_x + delta_y*delta_y);
+            if(dist != 0 && dist <= this.node[i].links.length/2+this.node[j].links.length/2){
+              fill(255, 0, 0);
+              var x_orientation = 0, y_orientation = 0;
+              if(this.node[i].position.x >= this.node[j].position.x){
+                x_orientation = 1;
+              }else{
+                x_orientation = -1;
+              }
+              if(this.node[i].position.y >= this.node[j].position.y){
+                y_orientation = 1;
+              }else{
+                y_orientation = -1;
+              }
+              this.node[i].position.x += x_orientation*delta_d;
+              this.node[i].position.y += y_orientation*delta_d;
+            }
+          }
+        }
+      }
+      ellipse(this.node[i].position.x, this.node[i].position.y, this.node[i].links.length);
+    }
   }
 }
 
@@ -90,7 +128,7 @@ class Node{
     this.label = label;
     this.weight = weight;
     this.links = [];
-    this.size = 0;
+    this.size = 20;
     this.position = {
       x: floor(random(this.size, windowWidth-this.size)),
       y: floor(random(this.size, windowHeight-this.size))
@@ -99,8 +137,8 @@ class Node{
 
   connect(node){
     this.links.push(node);
-    this.size = (this.links.length+1)*10;
-    node.size = (node.links.length+1)*10;
+    // this.size = (this.links.length+1)*10;
+    // node.size = (node.links.length+1)*10;
     this.position.x = floor(random(this.size, windowWidth-this.size));
     this.position.y = floor(random(this.size, windowHeight-this.size));
   }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
 var source = [], targets = [];
 
 function preload() {
-  var csv = loadStrings("data/dados_teste.csv", handleFile);
+  var csv = loadStrings("data/dados_teste_3.csv", handleFile);
 }
 
 function handleFile(file){
-	var companies = [];
-	for(var i = 1; i < file.length; i++){
-		companies[i-1] = file[i].split(',')[0];
-		source[i-1] = file[i].split(',')[1];
-		targets[i-1] = [];
-		for(var j = 2; j < file[i].split(',').length; j++){
-			targets[i-1][j-2] = file[i].split(',')[j].replace(/\"/g, "").replace(" ","");
+	//var companies = [];
+	for(var i = 0; i < file.length; i++){
+		// companies[i] = file[i].split(',')[0];
+		source[i] = file[i].split(',')[0].replace(/\"/g, "").replace(" ","").trim();
+		targets[i] = [];
+		for(var j = 1; j < file[i].split(',').length; j++){
+			targets[i][j-1] = file[i].split(',')[j].replace(/\"/g, "").replace(" ","").trim();
 		}
 	}
 }
@@ -25,6 +25,6 @@ function setup() {
 }
 
 function draw() {
-	background(0, 0, 0);
+  background(0, 0, 0);
 	graph.show();
 }
