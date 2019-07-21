330bc49606732a883982b30d9d43ef37f8580fa6
diegoserodio <diegocosta2113@gmail.com>
2019-07-21 02:04:20 -0300

    Data update and graph visualization tweaks

--- libraries/graph.js
+++ libraries/graph.js
@@ -1,31 +1,27 @@
 class Graph{
   constructor(){
-    this.source = [];
-    this.targets = [];
     this.node = [];
     this.link = [];
     this.label = [];
   }
 
-  generateGraphFromFile(source, targets){
-    this.source = source;
-    this.targets = targets;
+  generateGraphFromArray(source, targets){
     var nodes_qtn = 0;
-    for(var i = 0; i < this.source.length; i++){
-      if(!this.existsNode(this.source[i])){
-        this.label[this.source[i]]= nodes_qtn;
-        this.node[nodes_qtn] = new Node(this.source[i]);
+    for(var i = 0; i < source.length; i++){
+      if(!this.existsNode(source[i])){
+        this.label[source[i]]= nodes_qtn;
+        this.node[nodes_qtn] = new Node(source[i]);
         nodes_qtn++;
       }
-      for(var j = 0; j < this.targets[i].length; j++){
-        if(!this.existsNode(this.targets[i][j])){
-          this.label[this.targets[i][j]]= nodes_qtn;
-          this.node[nodes_qtn] = new Node(this.targets[i][j]);
+      for(var j = 0; j < targets[i].length; j++){
+        if(!this.existsNode(targets[i][j])){
+          this.label[targets[i][j]]= nodes_qtn;
+          this.node[nodes_qtn] = new Node(targets[i][j]);
           nodes_qtn++;
         }
       }
     }
-    this.createLinks();
+    this.createLinks(source, targets);
   }
 
   existsNode(label){
@@ -37,32 +33,59 @@ class Graph{
     return false;
   }
 
-  createLinks(){
-    for(var i = 0; i < this.source.length; i++){
-      for(var j = 0; j < this.targets[i].length; j++){
-        this.node[this.label[this.source[i]]].connect(this.node[this.label[this.targets[i][j]]]);
+  createLinks(source, targets){
+    var links_qtn = 0;
+    for(var i = 0; i < source.length; i++){
+      for(var j = 0; j < targets[i].length; j++){
+        this.node[this.label[source[i]]].connect(this.node[this.label[targets[i][j]]]);
         //------FOR NON-DIRECTED GRAPH UNCOMMENT LINE BELOW------
-        //this.node[this.label[this.targets[i][j]]].connect(this.node[this.label[this.source[i]]]);
+        //this.node[this.label[targets[i][j]]].connect(this.node[this.label[source[i]]]);
+      }
+    }
+    for(var i = 0; i < this.node.length; i++){
+      for(var j = 0; j < this.node[i].links.length; j++){
+        var node = this.node[i];
+        this.link[links_qtn] = new Link(node, node.links[j]);
+        links_qtn++;
+      }
+    }
+  }
+
+  setWeights(weights){
+    for(var i = 0; i < this.node.length; i++){
+      if(weights[i]!=0){
+        this.node[i].weight = weights[i];
+        this.node[i].size = (weights[i]*15)*(weights[i]*15);
+      }else{
+        this.node[i].weight = 0;
+        this.node[i].size = 20;
+        this.node[i].color = {
+          r: 100,
+          g: 100,
+          b: 100
+        };
       }
     }
   }
 
   show(){
-    var links_qtn = 0;
     for(var i = 0; i < this.node.length; i++){
       for(var j = 0; j < this.node[i].links.length; j++){
-        this.link[links_qtn] = new Link(this.node[i], this.node[i].links[j]);
-        stroke(255, 255, 255);
-        strokeWeight(this.link[links_qtn].weight);
-        line(this.node[i].position.x, this.node[i].position.y, this.node[i].links[j].position.x, this.node[i].links[j].position.y);
-        this.drawArrow(this.node[i], this.node[i].links[j]);
-        links_qtn++;
+        var node = this.node[i];
+        var link = this.link[j];
+        stroke(link.color.r, link.color.g, link.color.b);
+        strokeWeight(link.weight+1);
+        line(node.position.x, node.position.y, node.links[j].position.x, node.links[j].position.y);
+        this.drawArrow(node, node.links[j]);
       }
-      fill(255, 255, 255);
+    }
+    for(var i = 0; i < this.node.length; i++){
+      var node = this.node[i];
+      fill(node.color.r, node.color.g, node.color.b);
       stroke(0,0,0);
-      ellipse(this.node[i].position.x, this.node[i].position.y, this.node[i].size);
-      fill(0, 0, 255);
-      text(i, this.node[i].position.x-this.node[i].size/2, this.node[i].position.y-this.node[i].size/2);
+      ellipse(node.position.x, node.position.y, node.size);
+      fill(0, 255, 0);
+      text(i, node.position.x-node.size/2, node.position.y-node.size/2);
     }
     this.dominanceArea();
   }
@@ -80,7 +103,7 @@ class Graph{
       y_orientation = 1;
     }
     var theta = Math.atan(Math.abs((b.position.y-a.position.y)/(b.position.x-a.position.x)));
-    var radius = b.size-8;
+    var radius = b.size/2;
     var x = b.position.x + radius*Math.cos(theta)*x_orientation;
     var y = b.position.y + radius*Math.sin(theta)*y_orientation;
     fill(255, 0, 0);
@@ -91,44 +114,58 @@ class Graph{
   dominanceArea(){
     var delta_d = 1;
     for(var i = 0; i < this.node.length; i++){
+      var node_a = this.node[i];
+      var dominance = node_a.links.length*5;
       noStroke();
       fill(50, 50, 50);
-      if(this.node[i].links.length != 0){
+      if(dominance != 0){
         for(var j = i; j < this.node.length; j++){
-          if(this.node[j].links.length != 0){
-            var delta_x = this.node[i].position.x-this.node[j].position.x;
-            var delta_y = this.node[i].position.y-this.node[j].position.y;
+          var node_b = this.node[j];
+          if(node_b.links.length != 0){
+            var delta_x = node_a.position.x-node_b.position.x;
+            var delta_y = node_a.position.y-node_b.position.y;
             var dist = Math.sqrt(delta_x*delta_x + delta_y*delta_y);
-            if(dist != 0 && dist <= this.node[i].links.length/2+this.node[j].links.length/2){
+            if(dist != 0 && dist <= dominance/2+node_b.links.length/2){
               fill(255, 0, 0);
               var x_orientation = 0, y_orientation = 0;
-              if(this.node[i].position.x >= this.node[j].position.x){
+              if(node_a.position.x >= node_b.position.x){
                 x_orientation = 1;
               }else{
                 x_orientation = -1;
               }
-              if(this.node[i].position.y >= this.node[j].position.y){
+              if(node_a.position.y >= node_b.position.y){
                 y_orientation = 1;
               }else{
                 y_orientation = -1;
               }
-              this.node[i].position.x += x_orientation*delta_d;
-              this.node[i].position.y += y_orientation*delta_d;
+              node_a.position.x += x_orientation*delta_d;
+              node_a.position.y += y_orientation*delta_d;
+              if(node_a.position.x <= node_a.size/2){
+                node_a.position.x = node_a.size/2;
+              }else if(node_a.position.x >= windowWidth-node_a.size/2){
+                node_a.position.x = windowWidth-node_a.size/2;
+              }
+              if(node_a.position.y <= node_a.size/2){
+                node_a.position.y = node_a.size/2;
+              }else if(node_a.position.y >= windowHeight-node_a.size/2){
+                node_a.position.y = windowHeight-node_a.size/2;
+              }
             }
           }
         }
       }
-      ellipse(this.node[i].position.x, this.node[i].position.y, this.node[i].links.length);
+      //ellipse(node_a.position.x, node_a.position.y, dominance);
     }
   }
 }
 
 class Node{
-  constructor(label = "", weight = 1){
+  constructor(label = "", weight = 0, color = {r:255, g:255, b:0}){
     this.label = label;
     this.weight = weight;
     this.links = [];
-    this.size = 20;
+    this.size = 0;
+    this.color = color;
     this.position = {
       x: floor(random(this.size, windowWidth-this.size)),
       y: floor(random(this.size, windowHeight-this.size))
@@ -137,17 +174,14 @@ class Node{
 
   connect(node){
     this.links.push(node);
-    // this.size = (this.links.length+1)*10;
-    // node.size = (node.links.length+1)*10;
-    this.position.x = floor(random(this.size, windowWidth-this.size));
-    this.position.y = floor(random(this.size, windowHeight-this.size));
   }
 }
 
 class Link{
-  constructor(sourceNode, targetNode, weight = 1){
+  constructor(sourceNode, targetNode, weight = 0, color = {r:220, g:220, b:220}){
     this.weight = weight;
     this.sourceNode = sourceNode;
     this.targetNode = targetNode;
+    this.color = color;
   }
 }
